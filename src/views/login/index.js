ded025ca8a78081b72079089aeb33e383e8d5d1d
WS <liuyb@wondershare.cn>
2021-02-02 09:25:09 +0800

    feat:最新代码

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,12 +1,8 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
-import {Layout, Form, Icon, Input, Button, message, notification} from "antd";
-import io from 'socket.io-client';
-import wsUrl from '@/config';
+import {Layout, Form, Icon, Input, Button, message} from "antd";
 import request from "@/utils/request";
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
 import "./index.less";
 
 const FormItem = Form.Item;
@@ -29,7 +25,6 @@ class LoginForm extends Component {
 
     componentDidMount() {
         this.connectSocketJs();
-        console.log("测试缓存");
         // eslint-disable-next-line
         var keycloak = new Keycloak({
             url: 'http://10.10.17.41:8080/auth',
@@ -43,28 +38,43 @@ class LoginForm extends Component {
 
         keycloak.init({
             onLoad: 'check-sso',
-            silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+            silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+            pkceMethod: 'S256',
         }).success(function (auth) {
             if (auth) {
                 console.info('login Authenticated');
                 localStorage.setItem('auth', 'true');
+                window.keycloak = keycloak;
                 keycloak.loadUserProfile()
                     .then(function (profile) {
                         console.info('Authenticated keycloak');
                         console.info(JSON.stringify(keycloak, null, "  "));
+                        localStorage.setItem('token', keycloak.token);
                         console.info('Authenticated loadUserProfile');
                         console.info(JSON.stringify({
                             authenticated: true,
                             ...profile,
                         }, null, "  "));
                     }).catch(function () {
-                        alert('Failed to load user profile');
-                    });
+                    alert('Failed to load user profile');
+                });
+                var data = {
+                    message: 'sth to iframe1'
+                };
+                // 给子页面发送消息
+                document.getElementById('iframe1').contentWindow.postMessage(data, "*");
+                window.addEventListener('message', function (e) {
+                    console.log(e);
+                }, false);
+
+                keycloak.onAuthLogout = function () {
+                    console.info('listend onAuthLogout');
+                }
             } else {
                 console.info('login Not Authenticated');
                 keycloak.login();
             }
-        })
+        });
     }
 
     componentWillReceiveProps(nextProps) {
@@ -73,7 +83,6 @@ class LoginForm extends Component {
         if (login_result !== this.props.login_result) {
             if (login_result.success) {
                 message.success("登录成功");
-                this.connectMsgCenter(login_result.id);
                 localStorage.setItem('userInfo', JSON.stringify(login_result));
                 this.props.history.push('/home');
                 this.setState({
@@ -97,132 +106,9 @@ class LoginForm extends Component {
     componentWillUnmount() {
     }
 
-    connectSocketJs = () => {
-        // 本例搭配 websocket-spring-demo
-        // 可以实现正确连接
-        // let socket = new SockJS('http://127.0.0.1:8082/webSocket/webSocketEndPoint');
-        let socket = new SockJS('http://39.108.85.75:15674');
-        let stompClient = Stomp.over(socket);
-        let headers = {
-            username: 'admin',
-            password: 'admin'
-        };
-        stompClient.connect(headers, function (frame) {
-            console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/demo1/greetings', function (greeting) {
-
-            });
-            stompClient.subscribe('/topic/demo1/twoWays', function (greeting) {
-
-            });
-        });
-    };
-
-    /*
-    connectRabbitmqByStomp = () => {
-        let ws = new WebSocket('ws://39.108.85.75:15674');
-        let client = Stomp.over(ws);
-        // let client = Stomp.overWS('ws://39.108.85.75:61614');
-        // SockJS does not support heart-beat: disable heart-beats
-        client.heartbeat.outgoing = 0;
-        client.heartbeat.incoming = 0;
-        // Declare on_connect
-        let on_connect = function (x) {
-            client.subscribe("/queue/hello1", function (d) {
-                console.log(d.body);
-            });
-        };
-
-        // Declare on_error
-        let on_error = function () {
-            console.log('error');
-        };
-
-        // Conect to RabbitMQ
-        client.connect('guest', 'guest', on_connect, on_error, '/');
-    };
-    */
-
     refreshVerifyCode = () => {
         this.setState({
             verifyUrl: "/user/fetch_verify_code?r=" + Math.random()
-        });
-    };
-
-    openNotification = (msg) => {
-        notification.config({
-            placement: 'topRight',
-            top: 64,
-            duration: 6,
-        });
-
-        //只有在非登录或非注册页面，才显示通知消息
-        if (window.location.href.indexOf("/register") < 0 && window.location.href.indexOf("/login") < 0) {
-            notification['info']({
-                message: '温馨提示',
-                description: msg,
-                onClick: () => {
-                    console.log('Notification Clicked!');
-                },
-            });
-        }
-    };
-
-    connectMsgCenter = (userId) => {
-        if (window.socketConnected) {
-            window.socketConnected = null;
-        }
-        window.socketConnected = io.connect(wsUrl);
-        const t = new Date();
-        window.socketConnected && window.socketConnected.emit('login', userId);
-        console.log("User " + userId + " login at time: %s~", t);
-
-        // 后端推送来消息时
-        const _this = this;
-        window.socketConnected && window.socketConnected.on('message', function (msg) {
-            if (msg && msg.type && msg.userName && msg.text) {
-                let userInfo = localStorage.getItem("userInfo") ? localStorage.getItem("userInfo") : "";
-                if (userInfo) {
-                    userInfo = JSON.parse(userInfo);
-                    const userName = userInfo.user_name;
-                    if (userName !== msg.userName) {
-                        _this.openNotification(msg.text);
-                    }
-                }
-            } else {
-                console.log("window.socketConnected(receive msg)", window.socketConnected);
-                console.log("receive msg:" + msg);
-                _this.openNotification(msg);
-            }
-        });
-
-        //websocket服务端推送登录成功，此时需要将当前的socketId放入请求会话中
-        //通过向后端发送请求的方式实现
-        window.socketConnected && window.socketConnected.on('login_success', function (data) {
-            console.log("window.socketConnected(login_success)", window.socketConnected, data);
-            request
-                .post(
-                    "/user/set_socket_id_to_user_session",
-                    {
-                        socketId: window.socketConnected.id
-                    },
-                    {}
-                )
-                .then(function (res) {
-                    console.log("set_socket_id_to_user_session res", res);
-                    if (res.code === "0000") {
-                        console.log("set_socket_id_to_user_session success");
-                    } else {
-                        console.log("set_socket_id_to_user_session failure");
-                    }
-                });
-        });
-
-        window.socketConnected && window.socketConnected.on('redirect_to_login', function () {
-            console.log("window.socketConnected(redirect_to_login)", window.socketConnected);
-            window.socketConnected = null;
-            localStorage.clear();
-            _this.toPath("/login");
         });
     };
 
@@ -240,21 +126,10 @@ class LoginForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        /*this.props.form.validateFields((err, values) => {
-            this.setState({
-                throwErr: true
-            });
-            if (!err) {
-                this.handleLogin({
-                    userName: values.userName,
-                    password: values.password,
-                    verifyCode: values.verifyCode
-                });
-            }
-        });*/
         const {keycloak} = this.state;
         if (keycloak) {
             keycloak.logout();
+            keycloak.clearToken();
             localStorage.removeItem('auth');
         }
     };
